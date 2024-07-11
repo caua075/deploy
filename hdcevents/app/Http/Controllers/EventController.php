5af2e4fb9bfbe70693cf5aaa7aff6e4e66c9f60f
caua075 <caua.andradeg07@gmail.com>
2024-07-11 12:33:03 -0300

    create new views, routes and controllers

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App;
 use Illuminate\Http\Request;
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -45,6 +47,9 @@ class EventController extends Controller
             $event->image = $imageName;
         }
 
+        $user = auth()->user();
+        $event->user_id = $user->id;
+
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
@@ -53,6 +58,15 @@ class EventController extends Controller
     public function show($id) {
         $event = Event::findOrFail($id);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner = User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
+    }
+
+    public function dashboard() {
+        $user = auth()->user();
+        $events = $user->events;
+
+        return view('events.dashboard', ['events' => $events]);
     }
 }
